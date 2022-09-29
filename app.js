114be503fdfbed81d5f4979619af41e280628d53
epoirier1105 <poiriererin@gmail.com>
2022-09-28 17:25:59 -0700

    adding my website

---
+++ app.js
@@ -0,0 +1,28 @@
+//setting a variable for the humber menu
+let hamburger_menu = document.querySelector('#hamburger_menu');
+let mobile_menu = document.querySelector('#mobile_menu');
+let close = document.querySelector('#close');
+
+//click x and close the hamburger menu
+close.addEventListener('click', (event) => {
+    mobile_menu.classList.toggle("show_menu")
+    console.log("closed it");
+});
+
+//click hamurger menu and display drop down
+hamburger_menu.addEventListener('click', () => {
+    console.log("you clicked it")
+    mobile_menu.classList.toggle("show_menu");
+});
+
+//setting variables for form submit
+let form = document.querySelector('#contact_us');
+let div = document.querySelector('.title_content_container');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    console.log('submitted it');
+    form.style.display = 'none';
+    div.innerHTML += 'Thanks for contacting us!';
+})
+
